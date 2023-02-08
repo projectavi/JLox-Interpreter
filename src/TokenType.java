0ccaf5d38c15a89b4410e7c9063488178c0aad20
Aviraj Newatia <projectavi.exe@gmail.com>
2023-02-08 17:26:28 -0500

    Added token types in java enum

---
+++ src/TokenType.java
@@ -0,0 +1,20 @@
+enum TokenType {
+    // Single Character Tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One/Two Character Tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literal Type Tokens
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords for the language to reserve
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
